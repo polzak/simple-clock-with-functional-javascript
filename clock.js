c0068b5d79d93c83b4b5d2dd5f265e418f6f4785
polzak <polzak.yes@gmail.com>
2022-12-23 19:51:58 +0900

    Added doubleDigits function.

--- clock.js
+++ clock.js
@@ -6,44 +6,6 @@ const oneSec = () => 1000;
 
 const getCurrentTime = () => new Date();
 
-// const logClockTime = () => {
-//     var time = getClockTime();
-//     // clear();
-//     log(time);
-// }
-
-// const getClockTime = () => {
-    // var date = getCurrentTime();    
-
-    // chaining functions
-    // var time = civilianHours(appendAMPM(abstractClockTime(date)))
-
-    // use a compose function, as a higher order function, instead of chaning functions
-    // var time = compose(abstractClockTime, appendAMPM, civilianHours)(date);
-    
-    // time.hours = prependZero(time.hours);
-    // time.minutes = prependZero(time.minutes);
-    // time.seconds = prependZero(time.seconds);
-
-    // compose(getCurrentTime, abstractClockTime, appendAMPM, civilianHours);
-
-    //return `${time.hours}:${time.minutes}:${time.seconds} ${time.ampm}`
-// }
-
-// an improved version of the compose function
-// the form of a higher order function
-
-const compose = (...fns) =>
-    (arg) =>
-        fns.reduce((acc, f) => f(acc), arg);
-
-// function compose(...fns) {
-//     return function tripleFunctions(arg) {
-//         return fns[2](fns[1](fns[0](arg)));    
-//     }
-// }
-
-
 const abstractClockTime = date => (
     {
         hours: date.getHours(),
@@ -68,9 +30,26 @@ const civilianHours = clockTime => (
 
 const prependZero = n => (n >= 10) ? "" + n : "0" + n;
 
+const doubleDigits = clockTime => ({
+    ...clockTime,
+    hours: prependZero(clockTime.hours),
+    minutes: prependZero(clockTime.minutes),
+    seconds: prependZero(clockTime.seconds)
+})
+
+const compose = (...fns) => (arg) =>
+        fns.reduce((acc, f) => f(acc), arg);
+
 const startTicking = () => 
     setInterval(
-        compose(getCurrentTime, abstractClockTime, appendAMPM, civilianHours, log), 
+        compose(
+            getCurrentTime, 
+            abstractClockTime, 
+            appendAMPM,
+            civilianHours,
+            doubleDigits,
+            log), 
         oneSec());
-    
+        
+
 startTicking();
