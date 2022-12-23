772dbd542f16b0b8a04334238880399e1ec959b3
polzak <polzak.yes@gmail.com>
2022-12-23 20:19:22 +0900

    Cleaned the source file and added a link to the book Learning React in README file.

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 You can run `node clock.js` to see the clock on your console.  
 
-The source is from chapter 3 of Learning React by Alex Banks and Eve Porcello, and I modified some tiny parts of it.  
+The source is from the chapter 3 of [Learning React by Alex Banks and Eve Porcello](https://www.amazon.com/Learning-React-Modern-Patterns-Developing/dp/1492051721/), and I modified some tiny parts of it.  
 
 With this code sample, you can learn about introducing functional programming with Javascript.  
 
--- clock.js
+++ clock.js
@@ -6,45 +6,36 @@ const oneSec = () => 1000;
 
 const getCurrentTime = () => new Date();
 
-const abstractClockTime = date => (
-    {
+const abstractClockTime = date => ({
         hours: date.getHours(),
         minutes: date.getMinutes(),
         seconds: date.getSeconds()
-    }
-)
+    })
 
-const appendAMPM = clockTime => (
-    {
+const appendAMPM = clockTime => ({
         ...clockTime,
         ampm: (clockTime.hours <= 12) ? "AM" : "PM"
-    }
-)
+    })
 
-const civilianHours = clockTime => (
-    {
+const civilianHours = clockTime => ({
         ...clockTime,
         hours: (clockTime.hours <= 12) ? clockTime.hours : clockTime.hours - 12
-    }
-)
+    })
 
 const prependZero = n => (n >= 10) ? "" + n : "0" + n;
 
-const doubleDigits = handler => clockTime => (
-    {
+const doubleDigits = handler => clockTime => ({
     ...clockTime,
     hours: handler(clockTime.hours),
     minutes: handler(clockTime.minutes),
     seconds: handler(clockTime.seconds)
-    }
-)
+    })
 
 const formatter = clockTime => 
     `${clockTime.hours}:${clockTime.minutes}:${clockTime.seconds} ${clockTime.ampm}`
 
 // you can display the time with any other method than console.log, with this higher order function.
 const display = displayHandler => formattedTime => displayHandler(formattedTime);
-
 
 const compose = (...fns) => (arg) =>
         fns.reduce((acc, f) => f(acc), arg);
